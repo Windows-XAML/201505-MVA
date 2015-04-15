e080bf7fde5aea5694be5ae09cd6d8350f9d05d8
JerryNixon <jerry@online.jerrynixon.com>
2015-04-15 14:51:04 -0700

    Added SplashScreen

---
+++ SplashScreen/SplashScreen/SplashScreen-Blank/Views/Splash.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Sample.Views
+{
+    public sealed partial class Splash : Page
+    {
+        public Splash(Windows.ApplicationModel.Activation.SplashScreen splashScreen,
+               Nullable<Color> splashBackground = null, Uri splashImage = null)
+        {
+            this.InitializeComponent();
+
+            // defaults
+            if (splashBackground == null)
+                splashBackground = Colors.Red;
+            this.Background = new SolidColorBrush(Colors.Red);
+            if (splashImage == null)
+                splashImage = new Uri("ms-appx:///Assets/SplashScreen.png");
+            SplashImage.ImageOpened += (s, e) => Window.Current.Activate();
+            SplashImage.ImageFailed += (s, e) => Window.Current.Activate();
+            SplashImage.Source = new BitmapImage(splashImage);
+
+            // setup resize
+            Action resize = () =>
+            {
+                SplashImage.Stretch = Stretch.Uniform;
+                SplashImage.Height = splashScreen.ImageLocation.Height;
+                SplashImage.Width = splashScreen.ImageLocation.Width;
+                var t = new TranslateTransform
+                {
+                    X = splashScreen.ImageLocation.Left,
+                    Y = splashScreen.ImageLocation.Top,
+                };
+                SplashImage.RenderTransform = t;
+            };
+
+            // init
+            Window.Current.SizeChanged += (s, e) => resize();
+            resize.Invoke();
+        }
+    }
+}
---
+++ SplashScreen/SplashScreen/SplashScreen-Template10/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+
+namespace Template10
+{
+    sealed partial class App : Common.BootStrapper
+    {
+        public App() : base()
+        {
+            this.InitializeComponent();
+            this.SplashFactory = (s) => { return new Views.Splash(s); };
+        }
+
+        public override async Task OnLaunchedAsync(ILaunchActivatedEventArgs e)
+        {
+            // handle long-running tasks
+            await Task.Delay(5000);
+
+            // first screen
+            this.NavigationService.Navigate(typeof(Views.MainPage));
+        }
+    }
+}
