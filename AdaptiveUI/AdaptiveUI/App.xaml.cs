004bea80cc038a2e879b2bd9592cd7fccc404665
jbienzms <jbienzms@hotmail.com>
2015-04-22 15:43:08 -0500

    Added OrientationTrigger sample

--- AdaptiveUI/AdaptiveUI/App.xaml.cs
+++ AdaptiveUI/AdaptiveUI/App.xaml.cs
@@ -84,7 +84,8 @@ namespace AdaptiveUI
                 // configuring the new page by passing required information as a navigation
                 // parameter
                 // rootFrame.Navigate(typeof(MainPage), e.Arguments);
-                rootFrame.Navigate(typeof(WindowSizePage), e.Arguments);
+                // rootFrame.Navigate(typeof(WindowSizePage), e.Arguments);
+                rootFrame.Navigate(typeof(OrientationPage), e.Arguments);
             }
             // Ensure the current window is active
             Window.Current.Activate();
