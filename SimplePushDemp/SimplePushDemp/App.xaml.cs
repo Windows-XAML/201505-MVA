cc1dd0acbb98c65ac080efdc65c57780a6edb2ad
Andy Wigley <andy.wigley@outlook.com>
2015-05-03 13:17:44 +0100

    Simple Push Demo
    Basic example of handling push notifications

---
+++ SimplePushDemp/SimplePushDemp/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using SimplePushDemo.Helpers;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+
+namespace SimplePushDemp
+{
+    sealed partial class App : Common.BootStrapper
+    {
+        public App() : base()
+        {
+            this.InitializeComponent();
+        }
+
+        public override async Task OnLaunchedAsync(ILaunchActivatedEventArgs e)
+        {
+            this.NavigationService.Navigate(typeof(Views.MainPage));
+
+            var channelURI = await PushHandlingHelper.RegisterForPushAsync();
+            Debug.WriteLine(channelURI);
+        }
+    }
+}
---
+++ SimplePushDemp/SimplePushDemp/Helpers/PushHandlingHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking.PushNotifications;
+
+namespace SimplePushDemo.Helpers
+{
+    public static class PushHandlingHelper
+    {
+        public static async Task<string> RegisterForPushAsync()
+        {
+            var channel = await PushNotificationChannelManager.CreatePushNotificationChannelForApplicationAsync();
+            channel.PushNotificationReceived += channel_PushNotificationReceived;
+            return channel.Uri.ToString();
+        }
+
+        static void channel_PushNotificationReceived(PushNotificationChannel sender, PushNotificationReceivedEventArgs args)
+        {
+            string result = args.NotificationType.ToString();
+            switch (args.NotificationType)
+            {
+                case PushNotificationType.Badge:
+                    result += ": " + args.BadgeNotification.Content.GetXml();
+                    break;
+                case PushNotificationType.Raw:
+                    result += ": " + args.RawNotification.Content;
+                    break;
+                case PushNotificationType.Tile:
+                    result += ": " + args.TileNotification.Content.GetXml();
+                    break;
+                case PushNotificationType.TileFlyout:
+                    result += ": " + args.TileNotification.Content.GetXml();
+                    break;
+                case PushNotificationType.Toast:
+                    result += ": " + args.ToastNotification.Content.GetXml();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
