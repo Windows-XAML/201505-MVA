b392cb9897c757c41e7d4df1ebbb0413cacf4716
DanielMeixner <daniel.meixner@live.com>
2015-05-04 09:35:17 +0200

    Added AppService Demo:  Client App uses Encoder/Decoder Service from ServerApp. Make sure to runs ServerApp once to register service.

---
+++ AppService/AppService_EncoderDecoder/ServerApp/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ServerApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            ShowPFN();
+        }
+
+        // get the package family name
+        private async void ShowPFN()
+        {
+            var PFN = Windows.ApplicationModel.Package.Current.Id.FamilyName;
+            Debug.WriteLine("PFN=" + PFN);
+            await new MessageDialog(PFN).ShowAsync();
+        }
+    }
+}
