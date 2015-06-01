ebccdcb7014f1997efb51d47ccf2fdfac591ecb3
Andy Wigley <andy.wigley@outlook.com>
2015-06-01 18:06:55 +0100

    VideoInterstitial demo cleaned up

---
+++ VideoInterstitialAd/HelloWorld/ViewModels/TodoListViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace Template10.ViewModels
+{
+    public class TodoListViewModel : Mvvm.ViewModelBase
+    {
+        Repositories.TodoItemRepository _todoItemRepository;
+
+        public TodoListViewModel(Models.TodoList list)
+        {
+            _todoItemRepository = new Repositories.TodoItemRepository();
+
+            TodoList = list;
+            foreach (var item in list.Items)
+            {
+                Items.Add(new ViewModels.TodoItemViewModel(item));
+            }
+        }
+
+        private Models.TodoList _TodoList = default(Models.TodoList);
+        public Models.TodoList TodoList { get { return _TodoList; } set { Set(ref _TodoList, value); } }
+
+        private ObservableCollection<ViewModels.TodoItemViewModel> _Items = new ObservableCollection<TodoItemViewModel>();
+        public ObservableCollection<ViewModels.TodoItemViewModel> Items { get { return _Items; } private set { Set(ref _Items, value); } }
+
+        private ViewModels.TodoItemViewModel _SelectedItem = default(ViewModels.TodoItemViewModel);
+        public ViewModels.TodoItemViewModel SelectedItem { get { return _SelectedItem; } set { Set(ref _SelectedItem, value); SelectedItemIsSelected = (value != null); } }
+
+        private bool _SelectedItemIsSelected = default(bool);
+        public bool SelectedItemIsSelected { get { return _SelectedItemIsSelected; } set { Set(ref _SelectedItemIsSelected, value); } }
+
+        Mvvm.Command<string> _AddCommand = default(Mvvm.Command<string>);
+        public Mvvm.Command<string> AddCommand { get { return _AddCommand ?? (_AddCommand = new Mvvm.Command<string>(ExecuteAddCommand, CanExecuteAddCommand)); } }
+        private bool CanExecuteAddCommand(string title) { return true; }
+        private void ExecuteAddCommand(string title)
+        {
+            try
+            {
+                var index = Items.IndexOf(SelectedItem);
+                var item = new ViewModels.TodoItemViewModel(_todoItemRepository.Factory(title: title));
+                TodoList.Items.Insert((index > -1) ? index : 0, item.TodoItem);
+                Items.Insert((index > -1) ? index : 0, item);
+                SelectedItem = item;
+            }
+            catch { SelectedItem = null; }
+        }
+
+        Mvvm.Command<Models.TodoItem> _RemoveCommand = default(Mvvm.Command<Models.TodoItem>);
+        public Mvvm.Command<Models.TodoItem> RemoveCommand { get { return _RemoveCommand ?? (_RemoveCommand = new Mvvm.Command<Models.TodoItem>(ExecuteRemoveCommand, CanExecuteRemoveCommand)); } }
+        private bool CanExecuteRemoveCommand(Models.TodoItem param) { return SelectedItem != null; }
+        private void ExecuteRemoveCommand(Models.TodoItem param)
+        {
+            try
+            {
+                var index = Items.IndexOf(SelectedItem);
+                TodoList.Items.Remove(SelectedItem.TodoItem);
+                Items.Remove(SelectedItem);
+                SelectedItem = Items[index];
+            }
+            catch { SelectedItem = null; }
+        }
+    }
+}
---
+++ VideoInterstitialAd/HelloWorld/Views/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Template10.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            ViewModel = DataContext as ViewModels.MainPageViewModel;
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar= true;
+            Window.Current.SetTitleBar(AppTitle);
+        }
+
+        ViewModels.MainPageViewModel ViewModel { get; set; }
+
+        private async void TodoItem_ItemClicked(object sender, ItemClickEventArgs e)
+        {
+            TodoEditorDialog.DataContext = e.ClickedItem;
+            await TodoEditorDialog.ShowAsync();
+        }
+
+        private async void List_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            var textBlock = sender as TextBlock;
+            var data = textBlock.DataContext as ViewModels.TodoListViewModel;
+            ListEditorDialog.DataContext = data.TodoList;
+            ListEditorDialog.SecondaryButtonCommand = ViewModel.RemoveListCommand;
+            ListEditorDialog.SecondaryButtonCommandParameter = data;
+            await ListEditorDialog.ShowAsync();
+        }
+
+        private void TextBox_KeyDown(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            if (e.Key == Windows.System.VirtualKey.Enter 
+                && !string.IsNullOrEmpty(textBox.Text)
+                && textBox.Text.Length > 3)
+            {
+                e.Handled = true;
+                var list = textBox.DataContext as ViewModels.TodoListViewModel;
+                list.AddCommand.Execute(textBox.Text);
+                textBox.Text = string.Empty;
+                textBox.Focus(Windows.UI.Xaml.FocusState.Programmatic);
+            }
+        }
+    }
+}
